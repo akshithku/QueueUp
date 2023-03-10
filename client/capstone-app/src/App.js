ac419ae6bd23ca8f94949a33e2745703857e5bb9
akshithku <akshithkumar.karla@gmail.com>
2023-03-10 18:02:01 +0530

    set the server

--- client/capstone-app/src/App.js
+++ client/capstone-app/src/App.js
@@ -6,6 +6,7 @@ import DocList from './component/DocList';
 import PaymentPage from './component/Pay';
 import DocForm from './component/DocForm';
 import DocInfo from "./component/DocInfo";
+import DocRegi from './component/DocRegi';
 import { Route,Routes
  } from 'react-router-dom';
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path='/Pay' element={<PaymentPage/>}></Route>
         <Route path='/DocForm' element={<DocForm/>}></Route>
         <Route path='/DocInfo' element={<DocInfo/>}></Route>
+        <Route path='/DocRegi' element={<DocRegi/>}></Route>
       </Routes>
     </div>
   );
