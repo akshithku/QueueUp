4decac74170184db04f5ab63514242c9ff0b519f
akshithku <akshithkumar.karla@gmail.com>
2023-05-02 10:52:15 +0530

    some little changes

--- client/capstone-app/src/component/DocForm.js
+++ client/capstone-app/src/component/DocForm.js
@@ -37,7 +37,7 @@ const DocForm = () => {
       navigate("/DocInfo",{state:{TheUser:TheUser}})
     }
     else{
-      setUserEmail("");
+      // setUserEmail("");
     }
     console.log(TheUser)
   };
